8b96079606a6a2d8711a32fb34fb553f77d4ca32
KimHyeongSeob <hs8316@gmail.com>
2024-12-13 17:39:08 +0900

    12월 13일 자바1일차

---
+++ JavaBasic/src/day01/HelloJava.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		
+		// \n은 줄바꿈 명령
+		// 주석은 프로그램 코드 실행에 영향을 끼치지 않는다.
+		System.out.print("Hello 자바!!\n");
+		System.out.print("안녕하세요!?");
+		
+	}
+}
